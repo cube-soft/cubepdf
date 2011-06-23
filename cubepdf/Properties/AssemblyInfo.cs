2999d6b2e27823c4c657f3547e0ab6b471117e26
clown <tt.clown@gmail.com>
2011-06-23 17:32:12 +0900

    TIFF 形式の場合に解像度が選択できない不都合を修正．

--- cubepdf/Properties/AssemblyInfo.cs
+++ cubepdf/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.9.0")]
-[assembly: AssemblyFileVersion("0.9.9.0")]
+[assembly: AssemblyVersion("0.9.9.3")]
+[assembly: AssemblyFileVersion("0.9.9.3")]
