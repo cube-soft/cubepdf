011da6aed01b5ef8998169a8323c1c89e8f3d167
clown <tt.clown@gmail.com>
2012-05-17 02:42:26 +0900

    ポストプロセスの処理を修正

--- cubepdf-engine/PostProcess.cs
+++ cubepdf-engine/PostProcess.cs
@@ -21,6 +21,7 @@
 using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 
 namespace CubePDF {
     /* --------------------------------------------------------------------- */
@@ -33,21 +34,29 @@ namespace CubePDF {
         public bool Run(UserSetting setting) {
             if (setting.PostProcess == Parameter.PostProcesses.None) return true;
 
-            string path = setting.OutputPath;
-            if (!File.Exists(path)) {
-                path = Path.GetDirectoryName(path) + '\\' +
-                    Path.GetFileNameWithoutExtension(path) + "-001" + Path.GetExtension(path);
-            }
+            try
+            {
+                string path = setting.OutputPath;
+                if (!File.Exists(path))
+                {
+                    path = Path.GetDirectoryName(path) + '\\' +
+                        Path.GetFileNameWithoutExtension(path) + "-001" + Path.GetExtension(path);
+                }
+                if (!File.Exists(path)) return true; // 何らかの問題で変換に失敗しているので、スキップする。
 
-            try {
+                if (!IsExecutable(setting)) return false;
+
                 System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo();
                 System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                if (setting.PostProcess == Parameter.PostProcesses.Open) {
+                if (setting.PostProcess == Parameter.PostProcesses.Open)
+                {
                     psi.FileName = path;
                 }
-                else {
+                else
+                {
                     psi.FileName = setting.UserProgram;
-                    if (setting.UserArguments.Length > 0) {
+                    if (setting.UserArguments.Length > 0)
+                    {
                         string replaced = "\"" + path + "\"";
                         psi.Arguments = setting.UserArguments.Replace("%%FILE%%", replaced);
                     }
@@ -60,8 +69,12 @@ namespace CubePDF {
                 proc.StartInfo = psi;
                 proc.Start();
             }
-            catch (Exception err) {
+            catch (Exception err)
+            {
                 _messages.Add(new Message(Message.Levels.Error, err.Message));
+                _messages.Add(new Message(Message.Levels.Debug, String.Format("Type: {0}", err.GetType().ToString())));
+                _messages.Add(new Message(Message.Levels.Debug, String.Format("Source: {0}", err.Source)));
+                _messages.Add(new Message(Message.Levels.Debug, String.Format("StackTrace: {0}", err.StackTrace)));
                 return false;
             }
 
@@ -74,6 +87,85 @@ namespace CubePDF {
         public List<CubePDF.Message> Messages {
             get { return _messages; }
         }
+
+        /* ----------------------------------------------------------------- */
+        ///
+        /// IsExecutable
+        ///
+        /// <summary>
+        /// ポストプロセスが実行可能かどうかをチェックする。判別方法は、
+        /// Open が指定された場合は関連付けられているかどうか、
+        /// UserProgram が指定された場合は指定されたプログラムが存在して
+        /// いるかどうかで判別する。
+        /// </summary>
+        ///
+        /* ----------------------------------------------------------------- */
+        private bool IsExecutable(UserSetting setting)
+        {
+            if (setting.PostProcess == Parameter.PostProcesses.Open)
+            {
+                uint size = 0;
+                string ext = Parameter.Extension(setting.FileType);
+                AssocQueryString(AssocF.Verify, AssocStr.Executable, ext, null, null, ref size);
+                if (size == 0)
+                {
+                    // NOTE: 関連付けされていない場合は、単純にスキップする（エラーメッセージを表示しない）。
+                    // _messages.Add(new Message(Message.Levels.Error, String.Format("{0}: ファイルが関連付けられていません", ext)));
+                    _messages.Add(new Message(Message.Levels.Debug, String.Format("{0}: ファイルが関連付けられていません。", ext)));
+                    return false;
+                }
+            }
+            else if (setting.PostProcess == Parameter.PostProcesses.UserProgram && setting.UserProgram.Length > 0)
+            {
+                if (!File.Exists(setting.UserProgram))
+                {
+                    _messages.Add(new Message(Message.Levels.Error, String.Format("{0}: プログラムが見つかりませんでした。", setting.UserProgram)));
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        /* ----------------------------------------------------------------- */
+        //  Win32 APIs
+        /* ----------------------------------------------------------------- */
+        #region Win32APIs
+
+        [Flags]
+        enum AssocF
+        {
+            Init_NoRemapCLSID = 0x1,
+            Init_ByExeName = 0x2,
+            Open_ByExeName = 0x2,
+            Init_DefaultToStar = 0x4,
+            Init_DefaultToFolder = 0x8,
+            NoUserSettings = 0x10,
+            NoTruncate = 0x20,
+            Verify = 0x40,
+            RemapRunDll = 0x80,
+            NoFixUps = 0x100,
+            IgnoreBaseClass = 0x200
+        }
+
+        enum AssocStr
+        {
+            Command = 1,
+            Executable,
+            FriendlyDocName,
+            FriendlyAppName,
+            NoOpen,
+            ShellNewValue,
+            DDECommand,
+            DDEIfExec,
+            DDEApplication,
+            DDETopic
+        }
+
+        [DllImport("Shlwapi.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern uint AssocQueryString(AssocF flags, AssocStr str, string pszAssoc, string pszExtra, System.Text.StringBuilder pszOut, ref uint pcchOut);
+
+        #endregion
 
         /* ----------------------------------------------------------------- */
         //  変数定義
