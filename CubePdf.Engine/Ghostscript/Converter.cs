6a27a50270402f9d50161e2d9cef18468704f34d
clown <tt.clown@gmail.com>
2014-04-22 21:52:35 +0900

    Ghostscript/* をリファクタリング

--- CubePdf.Engine/Ghostscript/Converter.cs
+++ CubePdf.Engine/Ghostscript/Converter.cs
@@ -1,22 +1,22 @@
 ﻿/* ------------------------------------------------------------------------- */
-/*
- *  Ghostscript/Converter.cs
- *
- *  Copyright (c) 2009 CubeSoft, Inc. All rights reserved.
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see < http://www.gnu.org/licenses/ >.
- */
+///
+/// Ghostscript/Converter.cs
+///
+/// Copyright (c) 2009 CubeSoft, Inc. All rights reserved.
+///
+/// This program is free software: you can redistribute it and/or modify
+/// it under the terms of the GNU Affero General Public License as published
+/// by the Free Software Foundation, either version 3 of the License, or
+/// (at your option) any later version.
+///
+/// This program is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+/// GNU Affero General Public License for more details.
+///
+/// You should have received a copy of the GNU Affero General Public License
+/// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+///
 /* ------------------------------------------------------------------------- */
 using System;
 using System.Diagnostics;
@@ -30,7 +30,7 @@ namespace CubePdf.Ghostscript
     /// Converter
     ///
     /// <summary>
-    /// Ghostscript API のラッパークラス．
+    /// Ghostscript API のラッパークラスです。
     /// </summary>
     ///
     /* --------------------------------------------------------------------- */
